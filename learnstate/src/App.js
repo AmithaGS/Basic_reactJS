69eae8e67b4f0a1e55eef690523d856edbb1e560
AmithaGS <amithags.xworkz@gmail.com>
2024-12-27 11:36:26 +0530

    learnstate

---
+++ learnstate/src/App.js
@@ -0,0 +1,21 @@
+import react, { useState } from 'react'
+import Counter from './Counter';
+
+export default function App () {
+// let name = '';
+// state: 
+  const [name, setName] = useState('Neha')
+
+  const handleclick = () => {
+    // name = 'Akash';
+    // alert(name);
+    setName('Akash');
+  }
+  return (
+    <div>
+      {/* <h1>Hello {name}</h1>
+      <button onClick={handleclick}>Click</button> */}
+      <Counter />
+    </div>
+  )
+}
